339ff64a5d729bc8546e5f3baabed4ef739fdee1
John Dorman <dormanjohn68@gmail.com>
2023-12-09 13:23:32 -0700

    removed todo list

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,6 @@ use traceedb::function::TraceeDb;
 use std::env;
 use std::ffi::CString;
 
-// Todos for refactor:
-// 1. Implement dynamic dispatch, either by stack pointer or heap
-//    where Command trait interface compartmentalizes command functionality
-//    down to just Creating command and execute() for Step, Quit, etc. MOSTLY DONE
-//
-// 2. Make builders for constructing the debugger, as TraceeDb object.
-//    build out the target for it giving Option<Pid> DONE
-//
-// 3. Build out better main loop with Commands, waits
-//
-// Other things, Parser pattern
-
 fn main() {
     println!("NEXTDB DEBUGGER\nType \"help\" for command list!");
 
