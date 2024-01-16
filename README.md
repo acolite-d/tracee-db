4e316099f19d3682d3db6d50c73cd79841588471
John Dorman <dormanjohn68@gmail.com>
2024-01-15 18:02:50 -0700

    updated readme, updated arg parsing

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # tracee-db
+
+A x86_64 GNU/Linux debugger written in Rust. Associated with a series of blog posts found here.
+
+1. [Forays into Systems Programming: “Who Watches the Watchmen?”–Writing An GNU/Linux x86_64 Debugger with Rust and the Nix Crate](https://find.thedoorman.xyz/?p=305)
+2. [Writing a GNU/Linux x86_64 Debugger in Rust (Part 2): A Rust-ic Refactoring](https://find.thedoorman.xyz/?p=312)
+3. [Writing a GNU/Linux x86_64 Debugger in Rust (Part 3): Implementing Breakpoints with DWARF, the Gimli Crate, and Traps](https://find.thedoorman.xyz/?p=314)
+
+# Building and Running Debugger
+
+Building is done via `cargo`. Run it and specify an executable with the run subcommand, offering the arguments after double dash.
+
+```sh
+cargo build
+cargo run -- myexecutable
+```
+
--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use std::env;
 use std::fs;
 
 fn main() {
-    println!("NEXTDB DEBUGGER\nType \"help\" for command list!");
+    println!("TRACEEDB DEBUGGER\nType \"help\" for command list!");
 
     let mut args = env::args().skip(1).take(2);
     let elf_buf: Vec<u8>;
@@ -15,8 +15,8 @@ fn main() {
     let mut builder = TraceeDbg::builder();
 
     if let Some(prog) = args.next() {
-        let data = fs::read(prog.as_str()).expect("Given program not found, exiting");
-        let file = object::File::parse(&*data).expect("Failed to parse program as ELF, exiting");
+        elf_buf = fs::read(prog.as_str()).expect("Given program not found, exiting");
+        let file = object::File::parse(&*elf_buf).expect("Failed to parse program as ELF, exiting");
 
         let is_et_dyn = match file.kind() {
             ObjectKind::Executable => false,
@@ -24,12 +24,9 @@ fn main() {
             _ => panic!("Please provide an ELF executable of type ET_DYN or ET_EXEC!"),
         };
 
-        builder = builder.program(prog).is_position_independent(is_et_dyn);
-    }
-
-    if let Some(dbg_file) = args.next() {
-        elf_buf = fs::read(dbg_file).expect("failed to load file for symbols!");
-        builder = builder.dwarf_symbols(elf_buf.as_slice());
+        builder = builder.program(prog)
+            .is_position_independent(is_et_dyn)
+            .dwarf_symbols(&elf_buf.as_slice());
     }
 
     builder.build().run();
